95f2e07c5b4893268a53ac4b8bbb9918108800d2
Gustavo Ramírez <gustavo81988@gmail.com>
2016-11-13 18:50:58 -0400

    Project POST todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
-var _= require('underscore');
+var _          = require('underscore');
+var db         = require('./db.js');
 
 var app        = express();
 var PORT       = process.env.PORT || 3000;
@@ -48,6 +49,13 @@ app.get('/todos/:id',function(req, res){
 app.post('/todos',function(req, res){
     var body = _.pick(req.body,'description','completed');
 
+    db.todo.create(body).then(function(todo){
+       res.json(todo.toJSON()); 
+    }).catch(function(e){
+        res.status(400).json(e);
+    });
+
+    /*
     if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
         return res.status(400).send();
     }
@@ -61,6 +69,7 @@ app.post('/todos',function(req, res){
     todos.push(body);
 
     res.json(body);
+    */
 });
 
 app.delete('/todos/:id',function(req, res){
@@ -103,6 +112,9 @@ app.put('/todos/:id',function(req, res){
     
 });
 
-app.listen(PORT, function(){
-    console.log('Express listening on port' + PORT + '!');
+db.sequelize.sync().then(function(){
+    app.listen(PORT, function(){
+        console.log('Express listening on port' + PORT + '!');
+    });
 });
+
